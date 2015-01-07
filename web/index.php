f0c0351ea9bf191b754a850b98644b390479b507
Mat Gadd <mat@whiteoctober.co.uk>
2015-01-07 17:21:03 +0000

    Work out and pass the id of the latest to the view.

--- web/index.php
+++ web/index.php
@@ -6,9 +6,27 @@ $app = require_once __DIR__ . '/../app/bootstrap.php';
 
 $app['exceptionHandler']->install();
 
+$data = $app['client']->getDaily();
+
+$mostRecentEntryId = false;
+$mostRecentDate = null;
+foreach ($data as $entry) {
+    if ($mostRecentDate === null) {
+        $mostRecentDate = $entry->getUpdatedAt();
+        $mostRecentEntryId = $entry->getId();
+    } else {
+        $entryDate = $entry->getUpdatedAt();
+        if ($entryDate > $mostRecentDate) {
+            $mostRecentDate = $entryDate;
+            $mostRecentEntryId = $entry->getId();
+        }
+    }
+}
+
 echo $app['viewService']->render(
     'index.html.twig',
     array(
-        'daily' => $app['client']->getDaily(),
+        'daily'             => $data,
+        'mostRecentEntryId' => $mostRecentEntryId,
     )
 );
