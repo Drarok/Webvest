cb1c31590346acedd3b82597ab14f3f2b75d9e7f
Mat Gadd <mat@whiteoctober.co.uk>
2016-04-20 14:44:56 +0100

    Show previous day if in the post-midnight gap

--- web/index.php
+++ web/index.php
@@ -6,7 +6,13 @@ $app = require_once __DIR__ . '/../app/bootstrap.php';
 
 $app['exceptionHandler']->install();
 
-$data = $app['client']->getDaily();
+// If viewing before 07:00, presume it's a little after midnight and show
+// the previous day, which is more likely to have some data.
+$date = new DateTime();
+if ($date->format('H:i:s') < '07:00:00') {
+    $date->modify('-1 day');
+}
+$data = $app['client']->getDaily($date);
 
 $mostRecentEntryId = false;
 $mostRecentDate = null;
