2c484c201df1e407483749b7724298e945a42a5c
Mat Gadd <mat@whiteoctober.co.uk>
2016-05-09 12:37:30 +0100

    Move existing code to a controller

--- web/index.php
+++ web/index.php
@@ -6,33 +6,24 @@ $app = require_once __DIR__ . '/../app/bootstrap.php';
 
 $app['exceptionHandler']->install();
 
-// If viewing before 07:00, presume it's a little after midnight and show
-// the previous day, which is more likely to have some data.
-$date = new DateTime();
-if ($date->format('H:i:s') < '07:00:00') {
-    $date->modify('-1 day');
-}
-$data = $app['client']->getDaily($date);
+// TODO: Use a proper router.
+// $_SERVER['REQUEST_URI'] => string '/'
+// $_SERVER['REQUEST_URI'] => string '/target-hours'
 
-$mostRecentEntryId = false;
-$mostRecentDate = null;
-foreach ($data as $entry) {
-    if ($mostRecentDate === null) {
-        $mostRecentDate = $entry->getUpdatedAt();
-        $mostRecentEntryId = $entry->getId();
-    } else {
-        $entryDate = $entry->getUpdatedAt();
-        if ($entryDate > $mostRecentDate) {
-            $mostRecentDate = $entryDate;
-            $mostRecentEntryId = $entry->getId();
-        }
-    }
+$uri = $_SERVER['REQUEST_URI'] ?? '/';
+
+switch ($uri) {
+    case '/':
+        $controller = new Controller\IndexController($app);
+        break;
+
+    case '/target-hours':
+        $controller = new Controller\TargetHoursController($app);
+        break;
+
+    default:
+        $controller = new Controller\ErrorController($app, $uri);
+        break;
 }
 
-echo $app['viewService']->render(
-    'index.html.twig',
-    array(
-        'daily'             => $data,
-        'mostRecentEntryId' => $mostRecentEntryId,
-    )
-);
+echo $controller->render();
